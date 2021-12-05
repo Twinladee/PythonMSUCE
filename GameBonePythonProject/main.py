d7654b2132891bb01a95f2a7a2a6897888abf5bd
Twinladee <66009219+Twinladee@users.noreply.github.com>
2021-12-05 17:09:04 +0300

    Игральная кость
    Игральная кость в Питоне

---
+++ GameBonePythonProject/main.py
@@ -0,0 +1,15 @@
+from graph import *
+from tkinter import *
+
+mainWindow()
+windowSize(500, 500)
+rectangle(100, 100, 300, 300)
+rectangle(200, 200, 400, 400)
+
+line(300, 300, 400, 400)
+line(100, 100, 200, 200)
+line(400, 200, 300, 100)
+line(100, 300, 200, 400)
+
+
+mainloop()
